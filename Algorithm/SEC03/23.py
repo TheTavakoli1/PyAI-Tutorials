67abb7361a1784d7f7b58b50816900d61446c0ff
TheTavakoli1 <TheTavakoli1@gmail.com>
2025-03-27 17:11:45 +0330

    Some example

---
+++ Algorithm/SEC03/23.py
@@ -0,0 +1,10 @@
+# Print name with a space between name and last name
+
+name = input("Enter your name: ")
+last_name = input("Enter your last name: ")
+
+# print(name, last_name)
+
+print(name + " " + last_name)
+
+# print(f"{name} {last_name}")
