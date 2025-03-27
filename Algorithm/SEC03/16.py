d30cc70bd8c14f404c8f8a06533c6132b3774ff2
TheTavakoli1 <TheTavakoli1@gmail.com>
2025-03-27 11:53:28 +0330

    Circumference and area of a circle

---
+++ Algorithm/SEC03/16.py
@@ -0,0 +1,14 @@
+r = 10
+
+Area = 3.14 * r * r
+# Area = 3.14 * r **2
+Circumference = 2 * 3.14 * r
+
+# print(Area)
+# print(Circumference)
+
+print("Area: ", Area)
+print("Circumference: ", Circumference)
+
+print(f"Area: {Area}")
+print(f"Circumference: {Circumference}")
