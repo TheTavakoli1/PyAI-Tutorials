ce2215483cd67e5fb51542d24c6a1d3900ae868c
TheTavakoli1 <TheTavakoli1@gmail.com>
2025-03-30 11:28:23 +0330

    Negative, Positive or Zero

---
+++ Algorithm/SEC04/09.py
@@ -0,0 +1,12 @@
+# Negative, Positive or Zero
+number = int(input("Enter your number: "))
+
+if number == 0:
+    print("Zero")
+
+elif number > 0:
+    print("Positive")
+
+else:
+    print("Negative")
+    
