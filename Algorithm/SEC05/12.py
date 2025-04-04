bdd8bd92c0b8b612bf1d04f94a69353fae43671d
TheTavakoli1 <TheTavakoli1@gmail.com>
2025-04-04 19:10:30 +0330

    Divisible by 3 or 5

---
+++ Algorithm/SEC05/12.py
@@ -0,0 +1,6 @@
+for i in range(1, 11):
+    number = int(input(f'Enter number {i}: '))
+    if number % 3 == 0 or number % 5 == 0:
+        print(f'{number} is divisible by 3 or 5')
+    else:
+        print(f'{number} is not divisible by 3 or 5')
