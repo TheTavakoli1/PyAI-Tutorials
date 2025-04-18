34e1ee23d28768a2a5cdcd7cb1cd85554af1a44d
TheTavakoli1 <TheTavakoli1@gmail.com>
2025-04-18 11:38:33 +0330

    Add loop to take user input until 0 is entered

---
+++ Algorithm/SEC06/13.py
@@ -0,0 +1,12 @@
+# number = int(input("Enter you number: "))
+# while number != 0:
+#     number = int(input("Enter you number: "))
+# print("Done!!")    
+    
+
+
+while True:
+    number = int(input('Enter your number: '))
+    if number == 0:
+        break
+
