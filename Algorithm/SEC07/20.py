a380636dbf65f2464ae0065e453da7bcd8fc877e
TheTavakoli1 <TheTavakoli1@gmail.com>
2025-05-01 22:53:38 +0330

    Capitalize the first letter of the input name

---
+++ Algorithm/SEC07/20.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+name = name[0].upper() + name[1:]
+print(name)
+
+
+# name = input("Enter your name: ")
+# result = ""
+# for i in range(len(name)):
+#     if i == 0:
+#         result += name[i].upper()
+#     else:
+#         result += name[i]
+# print(result)
+   
