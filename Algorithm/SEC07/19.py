b3456b4d902b60f72fd875c2e8d1c790874ccd1f
TheTavakoli1 <TheTavakoli1@gmail.com>
2025-05-01 22:51:57 +0330

    Count vowels in input name

---
+++ Algorithm/SEC07/19.py
@@ -0,0 +1,20 @@
+# name = input("Enter your name: ")
+# wovel = "OoEeAaIiUu"
+# count = 0
+# for i in range(len(name)):
+#     if name[i] in wovel:
+#         count += 1
+
+# print(count)
+
+print('------------------------------')
+
+name = input("Enter your name: ")
+wovel = "OoEeAaIiUu"
+count = 0
+for i in name:
+    if i in wovel:
+        count += 1
+
+print(count)
+
