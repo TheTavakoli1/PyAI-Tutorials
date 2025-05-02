77a6751088a501c3bc8fff4ab6184580b53a6ad0
TheTavakoli1 <TheTavakoli1@gmail.com>
2025-05-02 10:27:58 +0330

    Demonstrate list creation and element addition using append()

---
+++ Algorithm/SEC08/05.py
@@ -0,0 +1,14 @@
+# Data Structure
+# List [] - ordered, mutable, allows duplicate elements
+
+# names = list()
+names = []
+
+names.append("John")
+names.append("Jane")
+names.append("Jim")
+names.append("Jill")
+
+print(names)
+print(len(names))
+print(type(names))
