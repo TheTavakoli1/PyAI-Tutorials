d0edda69f81637f2dd1ec91ae713d8f3a13d4665
TheTavakoli1 <TheTavakoli1@gmail.com>
2025-05-14 09:07:07 +0330

    add NumPy 2D array with mixed data types example

---
+++ DataAnalysis/SEC02/03.py
@@ -0,0 +1,9 @@
+# _list = [1, 2, 1.3, "Ahmad", True, False]
+# print(_list)
+
+import numpy as np
+X = np.array([[1, 2, 3], [4, 5.2, 6], ["7", 8, 9]])
+print(X)
+print(X.shape)
+print(X.dtype)
+print(X.ndim)
