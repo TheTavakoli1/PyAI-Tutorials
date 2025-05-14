b4c82f373c2d10b1b53e4a459504eec9257d1fb8
TheTavakoli1 <TheTavakoli1@gmail.com>
2025-05-14 09:15:46 +0330

    add examples of arange and linspace in NumPy

---
+++ DataAnalysis/SEC02/04.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+# print(list(range(1.2, 11, 0.5))) # Error
+# r = range(1,11)
+# print(r)
+
+# X = np.arange(0.1, 10, 0.3)
+# print(X)
+
+# X = np.arange(1, 11, 2, dtype=np.uint8)
+# X = np.arange(1, 11, dtype=np.uint8)
+X = np.arange(11, dtype=np.uint8)
+# print(X)
+
+
+X = np.linspace(1, 10, 5)
+# print(X)
+
